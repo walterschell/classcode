c1c0af99f0d4f5cf25172a1c89626d5efa4845b8
Walter <walter@walters-mbp.home>
2014-07-26 12:11:29 -0400

    finished demo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include "wregex.h"
+#include <iostream>
+#include <string>
 int main(int argc, char *argv[])
 {
+    std::string regex("\\((.*)\\),\\((.*)\\)");
+    std::string line = "asdfsdaf(part1),(part2)asdfasdf";
+    std::cout << "Regex: ->" << regex << "<-\n";
+    std::cout << "Line: ->" << line << "<-\n";
+    std::cout << "Matches:";
+    for (auto match : search(regex, line))
+    {
+        std::cout << "->" << match << "<- ";
+    }
+    std::cout << std::endl;
+    
+    
     return 0;
 }
