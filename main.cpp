2582996faf943f01a648e9958e6d31b2df95bfd9
Walter <walter@walters-mbp.home>
2014-07-29 18:35:57 -0400

    Demo works to pull data from modified example on OSX

--- main.cpp
+++ main.cpp
@@ -3,17 +3,21 @@
 #include <string>
 int main(int argc, char *argv[])
 {
-    std::string regex("\\((.*)\\),\\((.*)\\)");
-    std::string line = "asdfsdaf(part1),(part2)asdfasdf";
+    std::vector<std::string> lines = {"Spot 1,34QCH6325382059, 10KM, \"A Random spot on the globe\"",
+                                      "Spot 2,49VDC1933169122, 4000M, \"Another random spot on the globe\"",
+                                      "Spot 3,24XVK5252656544, 6.7KM, \"Yet another random spot on the globe\""};
+    std::string regex("^(.+?),(.+?),\\s*([\\d\\.]+)([kK]?[mM]),\\s*\"(.+)\"");
     std::cout << "Regex: ->" << regex << "<-\n";
-    std::cout << "Line: ->" << line << "<-\n";
-    std::cout << "Matches:";
-    for (auto match : search(regex, line))
+    for (auto line : lines)
     {
-        std::cout << "->" << match << "<- ";
+        std::cout << "Line: ->" << line << "<-\n";
+        std::cout << "Matches:";
+        for (auto match : search(regex, line))
+        {
+            std::cout << "->" << match << "<- ";
+        }
+        std::cout << std::endl;
     }
-    std::cout << std::endl;
-    
     
     return 0;
 }
